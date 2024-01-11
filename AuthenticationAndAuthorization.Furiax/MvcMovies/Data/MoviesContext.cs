6cb162dd76017ce3a7404a9ac000180f51ad5b83
furiax <carlmalfliet@proximus.be>
2024-01-11 20:52:04 +0100

    imported movies app with authorization and authentication

---
+++ AuthenticationAndAuthorization.Furiax/MvcMovies/Data/MoviesContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovies.Models;
+
+namespace MvcMovies.Data
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext(DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; } = default!;
+
+        public DbSet<Music>? Music { get; set; }
+        public DbSet<ErrorLog> ErrorLog { get; set; }
+    }
+}
---
+++ AuthenticationAndAuthorization.Furiax/MvcMovies/Models/ErrorLog.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcMovies.Models
+{
+    public class ErrorLog
+    {
+        public int Id { get; set; }
+        public string? LogLevel { get; set; }
+        public string? ErrorMessage { get; set; }
+        public string?  StackTrace { get; set; }
+        public DateTime LogTime { get; set; }
+    }
+}
