1e8b9203caa36405ca2e9fcc26ac675fb5e33159
iamnikitakostin <contact@nikitakostin.com>
2025-03-21 17:37:14 -0700

    readded folder

---
+++ AuthenticationAndAuthorization.iamnikitakostin/MVCSongs.iamnikitakostin/Log/DatabaseLogProvider.cs
@@ -0,0 +1,23 @@
+﻿using MVCSongs.Data;
+
+namespace MVCSongs.Log;
+
+public class DatabaseLogProvider : ILoggerProvider
+{
+    private readonly MVCSongsContext _context;
+
+    public DatabaseLogProvider(MVCSongsContext context)
+    {
+        _context = context;
+    }
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        return new DatabaseLogger(_context);
+    }
+
+    public void Dispose()
+    {
+
+    }
+}
---
+++ AuthenticationAndAuthorization.iamnikitakostin/MVCSongs.iamnikitakostin/Log/DatabaseLogger.cs
@@ -0,0 +1,39 @@
+﻿using MVCSongs.Data;
+
+namespace MVCSongs.Log;
+
+public class DatabaseLogger : ILogger
+{
+    private readonly MVCSongsContext _context;
+
+    public DatabaseLogger(MVCSongsContext context)
+    {
+        _context = context;
+    }
+
+    public IDisposable BeginScope<TState>(TState state) => null;
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return logLevel >= LogLevel.Information;
+    }
+
+    public void  Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+    {
+        if (formatter == null)
+            return;
+
+        string logMessage = formatter(state, exception);
+
+        _context.Errors.Add(new Models.Error
+        {
+            LogLevel = logLevel.ToString(),
+            Message = logMessage,
+            Timestamp = DateTime.Now
+        });
+
+        _context.SaveChanges();
+
+        Console.WriteLine($"[{logLevel}] {logMessage}");
+    }
+}
